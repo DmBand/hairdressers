87569cfa8ea1123a1e6a1bf1606cbe6c9bb1aa33
DmBand <d.bandysik@gmail.com>
2022-04-08 21:57:51 +0300

    add constraint styles for portfolio button

--- hairdressers_project/users_app/static/users_app/js/index.js
+++ hairdressers_project/users_app/static/users_app/js/index.js
@@ -1,6 +1,10 @@
 'use strict'
 
 // Кнопка добавления файлов в портфолио
+const submitBtn = document.querySelector('.portf-btn-sumbit');
+const btnWrapper = document.querySelector('.input__file-button');
+const text = document.querySelector('.input__file-button-text');
+
 
 let inputs = document.querySelectorAll('.input__file');
         Array.prototype.forEach.call(inputs, function (input) {
@@ -12,12 +16,26 @@ let inputs = document.querySelectorAll('.input__file');
                 if (this.files && this.files.length >= 1)
                     countFiles = this.files.length;
 
-                if (countFiles)
-                    label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+                if (countFiles) {
+                    if (countFiles > 20) {
+                        text.classList.add('red-text');
+                        label.querySelector('.input__file-button-text').innerText = 'Не более 20 файлов за раз (' + countFiles + ')' ;
+                        submitBtn.classList.add('none-class');
+                        btnWrapper.classList.add('add-width')
+                    } else {
+                        label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+                        if (submitBtn.classList.contains('none-class')) {
+                            submitBtn.classList.remove('none-class');
+                            text.classList.remove('red-text');
+                            btnWrapper.classList.remove('add-width')
+                        }
+                    }
+                }
                 else
                     label.querySelector('.input__file-button-text').innerText = labelVal;
             });
         });
+
 
 // Кнопка загрузки аватара
 const upload = document.querySelector('#input__file2');
