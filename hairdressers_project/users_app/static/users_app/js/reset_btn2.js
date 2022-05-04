41ff7cb5951f4d92e6f025cf1eb3b051bd330e56
DmBand <d.bandysik@gmail.com>
2022-05-04 13:41:14 +0300

    reset_btn2

---
+++ hairdressers_project/users_app/static/users_app/js/reset_btn2.js
@@ -0,0 +1,35 @@
+'use strict'
+
+
+// Кнопка очистки портфолио
+const delPortf = document.querySelector(".del-photo-text");
+const divYesNo = document.querySelector(".yes-no-container");
+const btnYes = document.getElementById('yes-reset');
+
+function confirmReset() {
+    delPortf.classList.toggle('active');
+    if (delPortf.classList.contains('active')) {
+        delPortf.innerText = 'Удалить аватар?'
+        btnYes.classList.add('yes-reset');
+        btnYes.innerText = 'Да';
+
+        const btnNo = document.createElement('div');
+        btnNo.classList.add('no-reset');
+        btnNo.innerText = 'Нет'
+        divYesNo.append(btnYes);
+        divYesNo.append(btnNo);
+
+        btnNo.addEventListener('click', confirmReset);
+
+    } else {
+            const btnNo = document.querySelector('.no-reset');
+            btnNo.remove();
+
+            btnYes.classList.remove('yes-reset');
+            btnYes.innerText = '';
+
+            delPortf.innerText = 'Удалить аватар';
+    }
+}
+
+delPortf.addEventListener('click', confirmReset)
