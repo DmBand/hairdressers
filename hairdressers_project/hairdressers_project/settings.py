31f68e36d1b0178185acb2f120c0141b04dd42ea
DmBand <d.bandysik@gmail.com>
2022-05-08 12:04:12 +0300

    env

--- hairdressers_project/hairdressers_project/settings.py
+++ hairdressers_project/hairdressers_project/settings.py
@@ -14,9 +14,7 @@ from pathlib import Path
 
 import environ
 
-env = environ.Env(
-
-)
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
