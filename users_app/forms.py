903bec8fd55e3e4225f7fdb8467e12795a88c484
DmBand <d.bandysik@gmail.com>
2022-08-08 10:17:59 +0300

    refactoring forms

--- users_app/forms.py
+++ users_app/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm, PasswordChangeForm
+from django.contrib.auth.forms import (UserCreationForm,
+                                       AuthenticationForm,
+                                       SetPasswordForm,
+                                       PasswordChangeForm)
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
@@ -10,7 +13,6 @@ from .models import Hairdresser, SimpleUser
 # main profile
 class RegistrationUserForm(UserCreationForm):
     """ Форма регистрации нового пользователя """
-
     # Валидатор для username
     username_validator = RegexValidator(
         regex=r'^[0-9a-zA-Z_-]*$',
@@ -43,18 +45,28 @@ class RegistrationUserForm(UserCreationForm):
         validators=[first_and_last_name_validator],
         widget=forms.TextInput(attrs={'class': 'reg-form-input'})
     )
-    email = forms.EmailField(label='Эл. адрес', widget=forms.EmailInput(attrs={'class': 'reg-form-input'}))
+    email = forms.EmailField(
+        label='Эл. адрес',
+        widget=forms.EmailInput(attrs={'class': 'reg-form-input'})
+    )
     password1 = forms.CharField(
         label='Пароль',
         validators=[password_validator],
         widget=forms.PasswordInput(attrs={'class': 'reg-form-input'}))
-    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs={'class': 'reg-form-input'}))
+    password2 = forms.CharField(
+        label='Повторите пароль',
+        widget=forms.PasswordInput(attrs={'class': 'reg-form-input'})
+    )
 
     class Meta:
         model = User
         fields = (
-            'username', 'first_name', 'last_name', 'email',
-            'password1', 'password2'
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2'
         )
 
     def clean_email(self):
@@ -79,20 +91,23 @@ class AddAvatarForm(forms.ModelForm):
 
 class LoginUserForm(AuthenticationForm):
     """ Форма авторизации пользователей """
-
-    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'reg-form-input'}))
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'reg-form-input'}))
+    username = forms.CharField(
+        label='Логин',
+        widget=forms.TextInput(attrs={'class': 'reg-form-input'})
+    )
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'reg-form-input'})
+    )
 
 
 class EditProfileForm(forms.ModelForm):
     """ Форма редактирования главного профиля """
-
     # Валидатор для имени и фамилии
     first_and_last_name_validator = RegexValidator(
         regex=r'^[a-zA-Zа-яёА-ЯЁ-]*$',
         message='Допускаются буквы а-яА-Я, a-zA-Z.'
     )
-
     first_name = forms.CharField(
         label='Имя',
         validators=[first_and_last_name_validator],
@@ -107,9 +122,9 @@ class EditProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = [
-            'first_name', 'last_name'
+            'first_name',
+            'last_name'
         ]
-
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'reg-form-input'}),
             'last_name': forms.TextInput(attrs={'class': 'reg-form-input'})
@@ -122,20 +137,17 @@ class ResetPasswordForm(SetPasswordForm):
     Форма создания нового пароля после сброса старого
     (без ввода старого пароля)
     """
-
     # Валидатор пароля
     password_validator = RegexValidator(
         regex=r'^[^а-яёА-ЯЁ]+$',
         message='Символы кириллицы (а-яА-Я) не допускаются.'
     )
-
     new_password1 = forms.CharField(
         label="Новый пароль",
         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
         strip=False,
         validators=[password_validator]
     )
-
     new_password2 = forms.CharField(
         label="Подтвердите пароль",
         strip=False,
@@ -145,20 +157,17 @@ class ResetPasswordForm(SetPasswordForm):
 
 class ChangePasswordForm(PasswordChangeForm):
     """ Форма изменения пароля (с вводом старого пароля) """
-
     # Валидатор пароля
     password_validator = RegexValidator(
         regex=r'^[^а-яёА-ЯЁ]+$',
         message='Символы кириллицы (а-яА-Я) не допускаются.'
     )
-
     new_password1 = forms.CharField(
         label="Новый пароль",
         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
         strip=False,
         validators=[password_validator]
     )
-
     new_password2 = forms.CharField(
         label="Подтвердите пароль",
         strip=False,
@@ -173,30 +182,49 @@ class CreatePortfolioForm(forms.ModelForm):
     class Meta:
         model = Hairdresser
         fields = [
-            'phone', 'city', 'skills',
-            'another_info', 'instagram', 'portfolio'
+            'phone',
+            'city',
+            'skills',
+            'another_info',
+            'instagram',
+            'portfolio'
         ]
         widgets = {
-            'phone': forms.NumberInput(attrs={'class': 'portf-form-input',
-                                              'placeholder': 'Пример: +375291112233',
-                                              'required': True}),
-            'city': forms.Select(attrs={'class': 'portfolio-city-select'}),
-            'skills': forms.CheckboxSelectMultiple(attrs={'class': 'portf-form-input-cb'}),
-            'another_info': forms.Textarea(attrs={'class': 'portfolio-textarea',
-                                                  'placeholder': 'Укажите доплнительную информацию о себе: '
-                                                                 'стоимость услуг, возмжен ли выезд к клиенту, '
-                                                                 'стаж, ссылки на электронные сертификаты и т.д.'}),
-            'instagram': forms.TextInput(attrs={'class': 'portf-form-input',
-                                                'placeholder': 'Никнейм (например: ivan123.1vanov)'}),
-            'portfolio': forms.FileInput(attrs={'multiple': True,
-                                                'class': 'input input__file',
-                                                'id': 'input__file'}),
+            'phone': forms.NumberInput(
+                attrs={
+                    'class': 'portf-form-input',
+                    'placeholder': 'Пример: +375291112233',
+                    'required': True}
+            ),
+            'city': forms.Select(
+                attrs={'class': 'portfolio-city-select'}
+            ),
+            'skills': forms.CheckboxSelectMultiple(
+                attrs={'class': 'portf-form-input-cb'}
+            ),
+            'another_info': forms.Textarea(
+                attrs={
+                    'class': 'portfolio-textarea',
+                    'placeholder': 'Укажите доплнительную информацию о себе: '
+                                   'стоимость услуг, возмжен ли выезд к клиенту, '
+                                   'стаж, ссылки на электронные сертификаты и т.д.'}
+            ),
+            'instagram': forms.TextInput(
+                attrs={
+                    'class': 'portf-form-input',
+                    'placeholder': 'Никнейм (например: ivan123.1vanov)'}
+            ),
+            'portfolio': forms.FileInput(
+                attrs={
+                    'multiple': True,
+                    'class': 'input input__file',
+                    'id': 'input__file'}
+            ),
         }
 
 
 class DeleteProfileForm(forms.Form):
     """ Форма удаления портфолио и профиля пользователя """
-
     code = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'del-profile-input',
         'autocomplete': 'off',
