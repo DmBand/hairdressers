7fc3da2d88bc386610feba8e557edff3b266649b
DmBand <d.bandysik@gmail.com>
2022-08-14 21:50:43 +0300

    hairdresser permission

--- drf_app/permissons.py
+++ drf_app/permissons.py
@@ -13,3 +13,9 @@ class IsOwner(permissions.BasePermission):
     """ Открывает доступ только владельцу """
     def has_object_permission(self, request, view, obj):
         return obj.username == request.user.username
+
+
+class IsHairdresserOwner(permissions.BasePermission):
+    """ Открывает доступ только владельцу (для парикмахеров) """
+    def has_object_permission(self, request, view, obj):
+        return obj.owner.username == request.user.username
