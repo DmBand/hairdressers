5f2dc2bd2e3573cb41722c2057d5cff6b28c6126
DmBand <d.bandysik@gmail.com>
2022-09-01 20:51:22 +0300

    displaying a photo when requested by a hairdresser

--- drf_app/services.py
+++ drf_app/services.py
@@ -2,7 +2,10 @@ import base64
 import os
 import random
 
-from hairdressers_project.settings import MEDIA_ROOT
+from hairdressers_project.settings import (MEDIA_ROOT,
+                                           MEDIA_URL,
+                                           HOST_FOR_API,
+                                           DEBUG)
 from users_app.services import (check_number_of_files_in_portfolio,
                                 MAX_COUNT,
                                 compress_images_in_portfolio)
@@ -11,7 +14,7 @@ from users_app.services import (check_number_of_files_in_portfolio,
 def get_images(images: list, username: str) -> None or dict:
     errors = {
         'count': 0,
-        'message': 'Сломанный поток данных при чтении файла изображения'
+        'message': 'Поврежденный поток данных при чтении файла изображения'
     }
     b_images = [str.encode(img) for img in images if img]
     check_number_of_files_in_portfolio(
@@ -24,7 +27,7 @@ def get_images(images: list, username: str) -> None or dict:
     for index, value in enumerate(b_images):
         files = os.listdir(directory)
         while True:
-            name = f'img_{random.randrange(MAX_COUNT+1)}.jpg'
+            name = f'img_{random.randrange(MAX_COUNT + 1)}.jpg'
             if name not in files:
                 break
         try:
@@ -44,3 +47,22 @@ def get_images(images: list, username: str) -> None or dict:
     else:
         if errors['count'] > 0:
             return errors
+
+
+def get_photo_urls(username: str) -> dict or None:
+    """ Получение ссылок на все фото в портфолио """
+
+    if not DEBUG:
+        host = HOST_FOR_API
+    else:
+        host = 'http://127.0.0.1:8000'
+
+    directory = f'{MEDIA_ROOT}/portfolio/{username}'
+    try:
+        files = [f for f in os.listdir(directory)]
+    except FileNotFoundError:
+        return
+    else:
+        urls_list = [f'{host}{MEDIA_URL}portfolio/{username}/{f}' for f in files]
+        urls = {'urls': urls_list}
+        return urls
